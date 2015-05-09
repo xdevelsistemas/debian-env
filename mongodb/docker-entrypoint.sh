475f72ee2e0491fa889fffb7a2676245484b402d
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-05-09 05:53:09 -0300

    remove change owner and gosu command , problem with change permission

--- mongodb/docker-entrypoint.sh
+++ mongodb/docker-entrypoint.sh
@@ -6,14 +6,15 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'mongod' ]; then
-	chown -R mongodb /data/db
+	#chown -R mongodb /data/db
 
 	numa='numactl --interleave=all'
 	if $numa true &> /dev/null; then
 		set -- $numa "$@"
 	fi
 
-	exec gosu mongodb "$@"
+	#exec gosu mongodb "$@"
+	exec "$@"
 fi
 
 exec "$@"
